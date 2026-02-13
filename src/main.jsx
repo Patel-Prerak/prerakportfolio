59f58f43ed3d50da541eade01bb881905d1a82bf
Prearak Patel <patelprerak435@gmail.com>
2026-02-13 19:32:42 +0530

    chore: add version log to debug deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('DEPLOYMENT_VERSION: 2026-02-13-FIX-LOGO'); // Debugging deployment
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
